24e173b24c20daaee96a33f79b1cb3eebb5c82f2
rebhuhnc <rebhuhnc@oregonstate.edu>
2016-09-12 11:15:22 -0700

    Removed gtest and attempts at catkin_make

---
+++ src/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/aadi1/Source/carrie_lib/src
+# Build directory: /home/aadi1/Source/carrie_lib/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ src/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/aadi1/Source/carrie_lib/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/aadi1/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/aadi1/catkin_ws/devel;/opt/ros/indigo" != "" else []
